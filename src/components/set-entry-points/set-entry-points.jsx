0b1e232b0fa739bca7623898b2580432a828e712
Eron Tancioco <etancioco@crmonline.com.au>
2023-07-15 14:13:47 +0800

    Add clicking implementation to the table

--- src/components/set-entry-points/set-entry-points.jsx
+++ src/components/set-entry-points/set-entry-points.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useWizard } from "react-use-wizard";
 
+import "./set-entry-points.scss";
+
 const SetEntryPoints = (props) => {
     const {
         isLoading,
@@ -13,29 +15,47 @@ const SetEntryPoints = (props) => {
         goToStep,
         handleStep,
     } = useWizard();
-    const [ entryPoints, setEntryPoints ] = useState([]);
+    const [entryPoints, setEntryPoints] = useState([]);
+    const [clickedCells, setClickedCells] = useState([]);
 
-    const handleCellClick = (e) => {
-        const cellCoordinates = [e.target.parentElement.parentElement.rowIndex, e.target.parentElement.cellIndex];
-        
+    const handleCellClick = (rowIndex, columnIndex) => {
+        const cell = { rowIndex, columnIndex };
+        const isCellClicked = clickedCells.some(
+            clickedCell => clickedCell.rowIndex === rowIndex && clickedCell.columnIndex === columnIndex
+        );
 
-        if (entryPoints < props.numEntryPoints) {
-            setEntryPoints(prevEntryPoints => [...prevEntryPoints, cellCoordinates]);
+        // Check if a cell is clicked so we can toggle it.
+        if (isCellClicked) {
+            setClickedCells(clickedCells.filter(clickedCell => !(clickedCell.rowIndex === rowIndex && clickedCell.columnIndex === columnIndex)));
+        } else {
+            // If the is cell is not yet clicked, we should check if have the desired amount of entry points
+            if (clickedCells.length < props.numEntryPoints) {
+                setClickedCells([...clickedCells, cell]);
+            }
         }
-    }
+    };
 
     return (
-        <div>
+        <div className="set-entry-points">
             <h1>Select the entry points of the parking lot</h1>
             <table border={1} width={100}>
                 <tbody>
                     {props.parkingMap.map((row, rowIndex) => (
                         <tr key={rowIndex}>
-                            {row.map((slotType, columnIndex) => (
-                                <td key={columnIndex}>
-                                    <button onClick={handleCellClick}></button>
-                                </td>
-                            ))}
+                            {row.map((slotType, columnIndex) => {
+                                const isCellClicked = clickedCells.some(
+                                    clickedCell => clickedCell.rowIndex === rowIndex && clickedCell.columnIndex === columnIndex
+                                );
+
+                                return (
+                                    <td
+                                        key={columnIndex}
+                                        className={isCellClicked ? 'clicked' : ''}
+                                        onClick={() => handleCellClick(rowIndex, columnIndex)}
+                                    >
+                                    </td>
+                                );
+                            })}
                         </tr>
                     ))}
                 </tbody>
